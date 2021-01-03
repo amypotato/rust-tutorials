34b4068a41e2b4cd2489d268d1df535315cb6f0f
Amy Molloy <amypotato@icloud.com>
2021-01-03 16:01:23 +0000

    Read from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world2!");
+    println!("Guess the Number!");
+    println!("Please input your guess:");
 
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
 
+    println!("You guessed {}", guess)
 }
