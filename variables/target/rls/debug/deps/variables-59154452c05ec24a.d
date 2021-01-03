290710a3c1827225d690997ba528a5ea04152b00
Amy Molloy <amypotato@icloud.com>
2021-01-03 17:03:33 +0000

    Move guessing games into subfolder New variables project

---
+++ variables/target/rls/debug/deps/variables-59154452c05ec24a.d
@@ -0,0 +1,5 @@
+/home/amy/Dev/rust/rust-tutorials/variables/target/rls/debug/deps/variables-59154452c05ec24a.rmeta: src/main.rs
+
+/home/amy/Dev/rust/rust-tutorials/variables/target/rls/debug/deps/variables-59154452c05ec24a.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/rls/debug/deps/variables-c579acb3d8955f38.d
@@ -0,0 +1,5 @@
+/home/amy/Dev/rust/rust-tutorials/variables/target/rls/debug/deps/variables-c579acb3d8955f38.rmeta: src/main.rs
+
+/home/amy/Dev/rust/rust-tutorials/variables/target/rls/debug/deps/variables-c579acb3d8955f38.d: src/main.rs
+
+src/main.rs:
