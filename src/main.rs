963032edb61c72d88e1c31791707ea7c69ae5816
Amy Molloy <amypotato@icloud.com>
2021-01-03 16:21:53 +0000

    Cast, Pattern Matching, Rand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the Number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
     println!("Please input your guess:");
 
     let mut guess = String::new();
@@ -9,5 +14,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("failed to read line");
 
-    println!("You guessed {}", guess)
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed {}", guess);
+    println!("The secret number is: {}", secret_number);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
